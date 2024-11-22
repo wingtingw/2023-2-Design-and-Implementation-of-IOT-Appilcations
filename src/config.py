36d6213ccb8845f19d9cde5919b047d49d06c60a
Ying-Ting, Wu <137368318+wingtingw@users.noreply.github.com>
2024-11-22 11:02:22 +0800

    src

---
+++ src/config.py
@@ -0,0 +1,6 @@
+### LineBot Information ###
+ChannelAccessToken = 'fYBVUaaLiVTDXCmQ8DRFmzEwHQsO8L49PArnami9qk/YtVNI2ZD8v7lO2UrJoeQxy11MNph3BjcijtAn8gt8L4dYP261gXkyG0DuVeuXp14XnV6EDoTQdLf0SU/pkX8/y4uKbWCG+l+/G8vn/v4SOwdB04t89/1O/w1cDnyilFU='
+ChannelSecret = 'a2bf17fbbbc3ef866ae74f296c499877'
+
+### Line ID File Path ###
+idfilePath = './userIdFile'
